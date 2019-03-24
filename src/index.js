179afdb42d8ae7db20cd3fd035c43a310a0ad21b
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-03-24 19:22:46 +0300

    feat: create function getZerosCount which counts trailing zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let baseArr = [];
+  let num = 2;      
+  let baseObj = {};
+  let sum = [];
+
+  while (base > 1) {
+    if(base % num == 0) {
+      base = base / num;
+      if (baseArr.indexOf(num) == -1) {
+        baseArr.push(num);
+      }
+      baseObj.hasOwnProperty(num) ? baseObj[num]++ : (baseObj[num] = 1);
+    } else {
+      num++;
+    }
+  }
+
+  for (let i = 0; i < baseArr.length; i++) {
+    let currentNum = baseArr[i]; 
+    let x = baseArr[i];
+    let result = 0;
+    while ( number > currentNum)  {
+     result += Math.floor(number / currentNum);
+      currentNum *= x;
+    }
+    sum.push(Math.floor(result / baseObj[x]));
+  }
+  return Math.min(...sum);
 }
+
